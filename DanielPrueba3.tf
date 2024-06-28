6cb0f6073cba1263546cb03331b84c4298548966
dcontrerasuduoc <d.contrerasu@duocuc.cl>
2024-06-27 22:30:50 -0400

    Update DanielPrueba3.tf

--- DanielPrueba3.tf
+++ DanielPrueba3.tf
@@ -11,35 +11,32 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 1. Crear VPC
-module "my_vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.1.0"
-
+# Crear una VPC
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+ 
   name = "my-vpc"
   cidr = "10.0.0.0/16"
-  azs  = ["us-east-1a", "us-east-1b", "us-east-1c"]
-
+ 
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
+ 
   enable_nat_gateway = true
   enable_vpn_gateway = false
-
+ 
   tags = {
-    Terraform   = "true"
+    Terraform = "true"
     Environment = "prd"
   }
 }
-
-# 2. Crear Security Group
-resource "aws_security_group" "web_sg" {
-  name        = "web_sg"
-  description = "Security group for HTTP, HTTPS, SSH access"
-  vpc_id      = module.my_vpc.vpc_id
+# Crear un Security Group
+resource "aws_security_group" "allow_traffic" {
+  name        = "allow_traffic"
+  description = "Allow traffic on ports 80, 443, and 22"
+  vpc_id      = module.vpc.vpc_id
 
   ingress {
-    description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -47,7 +44,6 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
-    description = "Allow HTTPS"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -55,103 +51,130 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
-    description = "Allow SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
-    Name = "web_sg"
+    Name = "allow_traffic"
   }
 }
 
-# 3. Crear Bucket S3 y subir archivo index.php
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"
+# Crear un bucket S3 y copiar archivo index.php
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-website-bucket"
   acl    = "private"
+
+  tags = {
+    Name        = "my-website-bucket"
+    Environment = "prd"
+  }
 }
 
 resource "aws_s3_bucket_object" "index_php" {
-  bucket = aws_s3_bucket.my_bucket.bucket
+  bucket = aws_s3_bucket.bucket.bucket
   key    = "index.php"
   source = "index.php"
+  acl    = "public-read"
 }
 
-# 4. Crear instancias EC2
-resource "aws_instance" "web_servers" {
+# Lanzar 3 instancias EC2 en diferentes AZs
+resource "aws_instance" "web" {
   count         = 3
-  ami           = "ami-0abcdef1234567890"
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   key_name      = "vockey"
-  subnet_id     = element(module.my_vpc.public_subnets, count.index)
 
-  security_groups = [aws_security_group.web_sg.id]
+  subnet_id = element(module.vpc.public_subnets, count.index)
+  security_groups = [aws_security_group.allow_traffic.name]
 
   user_data = <<-EOF
               #!/bin/bash
+              sudo yum update -y
               sudo yum install -y httpd php
-              systemctl start httpd
-              systemctl enable httpd
-              mkdir /mnt/efs_mount
-              mount -t efs ${aws_efs_file_system.my_efs.file_system_id}:/ /mnt/efs_mount
-              cp /mnt/efs_mount/index.php /var/www/html/
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+              aws s3 cp s3://${aws_s3_bucket.bucket.bucket}/index.php /var/www/html/
               EOF
+
+  tags = {
+    Name = "WebServer-${count.index}"
+  }
 }
 
-# 5. Crear EFS
-resource "aws_efs_file_system" "my_efs" {
+# Crear un volumen EFS y montarlo en las instancias EC2
+resource "aws_efs_file_system" "efs" {
   creation_token = "my-efs"
-  performance_mode = "generalPurpose"
-
   tags = {
     Name = "my-efs"
   }
 }
 
-# 6. Crear ALB
-resource "aws_lb" "my_lb" {
-  name               = "my-lb"
+resource "aws_efs_mount_target" "efs_mount" {
+  count          = 3
+  file_system_id = aws_efs_file_system.efs.id
+  subnet_id      = element(module.vpc.public_subnets, count.index)
+  security_groups = [aws_security_group.allow_traffic.id]
+}
+
+# Crear un Load Balancer y adjuntar las instancias
+resource "aws_lb" "alb" {
+  name               = "my-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.web_sg.id]
-  subnets            = module.my_vpc.public_subnets
+  security_groups    = [aws_security_group.allow_traffic.id]
+  subnets            = module.vpc.public_subnets
 
   enable_deletion_protection = false
 
   tags = {
-    Name = "my-lb"
+    Name = "my-alb"
   }
 }
 
-resource "aws_lb_target_group" "web_target_group" {
-  name     = "web-target-group"
+resource "aws_lb_target_group" "target_group" {
+  name     = "my-targets"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = module.my_vpc.vpc_id
+  vpc_id   = module.vpc.vpc_id
 
   health_check {
-    path = "/"
-    port = 80
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "my-targets"
   }
 }
 
-resource "aws_lb_listener" "web" {
-  load_balancer_arn = aws_lb.my_lb.arn
-  port              = 80
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.web_target_group.arn
+    target_group_arn = aws_lb_target_group.target_group.arn
   }
 }
 
-resource "aws_lb_target_group_attachment" "web_attachment" {
-  count             = 3
-  target_group_arn  = aws_lb_target_group.web_target_group.arn
-  target_id         = aws_instance.web_servers[count.index].id
-  port              = 80
+resource "aws_lb_target_group_attachment" "target_attachment" {
+  count            = 3
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = element(aws_instance.web.*.id, count.index)
+  port             = 80
 }
-
