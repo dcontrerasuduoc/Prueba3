ea57fe0f271999a66e3c4a179d33997dc7f43778
dcontrerasuduoc <d.contrerasu@duocuc.cl>
2024-06-29 01:01:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Sitio Daniel Contreras</title>
 </head>
 <body>
-<h1>Welcome to my website</h1>
+<h1>Bienvenidos a mi web</h1>
 <p>Now hosted on: <?php echo gethostname(); ?></p>
 <p><?php
 $my_current_ip=exec("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'");
